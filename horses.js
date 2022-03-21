3a364ad11795aae387ad374b51a7cc175d00d1aa
Kaneka Miller <kmmiller418@gmail.com>
2022-03-21 13:23:35 -0400

    Add horses function

--- horses.js
+++ horses.js
@@ -1,7 +1,21 @@
-let horses = 9;
+let horses = 1;
 
 if (horses > 8) {
     console.log("There isn't enough room for all these horses!");
 } else {
     console.log("There is plenty of room in the stable");
 }
+
+
+// as a function
+
+function stableCalculator(numHorses) {
+    if (numHorses > 8) {
+        return("There isn't enough room for all these horses!");
+    } else {
+        return("There is plenty of room in the stable");
+    }
+}
+
+console.log(stableCalculator(9));
+console.log(stableCalculator(4));
