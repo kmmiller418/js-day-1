5e2b3c49d4491a4886388dba697dd01a776edd69
Kaneka Miller <kmmiller418@gmail.com>
2022-03-21 13:58:21 -0400

    Add helper function and storeOpen function

--- store-hours.js
+++ store-hours.js
@@ -7,11 +7,34 @@ var time;
 // holiday = false;
 
 if (weekend || holiday) {
-    console.log("The store is closed")
+    console.log("The store is closed");
 } else {
     if (time < 18) {
-        console.log("The store is open for business.")
+        console.log("The store is open for business.");
     } else {
-        console.log("The store is closed.")
+        console.log("The store is closed.");
+    }
+}
+
+//AS FUNCTION
+
+function storeOpen(weekend, holiday, time){
+    if (weekend || holiday) {
+        return("The store is closed");
+    } else {
+        if (time < 18) {
+            return("The store is open for business.");
+        } else {
+            return("The store is closed.");
+        }
+    }
+}
+
+//BONUS HELPER FUNCTION
+function isWeekend(day){
+    if (day == 'Saturday' || day == 'Sunday') {
+        return true;
+    } else {
+        return false;
     }
 }
