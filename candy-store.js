3e489b2401cb3587b8e204461f677cf7f169f9f1
Kaneka Miller <kmmiller418@gmail.com>
2022-03-21 13:41:10 -0400

    Rewrite candyStore as function

--- candy-store.js
+++ candy-store.js
@@ -19,18 +19,41 @@ var candyName;
 
 switch (candySize) {
     case 'small':
-        console.log("This " + candyName + " costs $1.00")
+        console.log("This " + candyName + " costs $1.00");
         break;
     case 'medium':
-        console.log("This " + candyName + " costs $1.75")
+        console.log("This " + candyName + " costs $1.75");
         break;
     case 'large':
-        console.log("This " + candyName +  " costs $2.50")
+        console.log("This " + candyName +  " costs $2.50");
         break;
     case 'jumbo':
-        console.log("This " + candyName + " costs $4.75")
+        console.log("This " + candyName + " costs $4.75");
         break;
     default: 
-        console.log("Associate please look up price and check if valid.")
+        console.log("Associate please look up price and check if valid.");
 }
 
+
+//AS A FUNCTION
+
+function candyStore(candyName, candySize){
+    switch (candySize) {
+        case 'small':
+            return("This " + candyName + " costs $1.00");
+        case 'medium':
+            return("This " + candyName + " costs $1.75");
+        case 'large':
+            return("This " + candyName +  " costs $2.50");
+        case 'jumbo':
+            return("This " + candyName + " costs $4.75");
+        default: 
+            return("Associate please look up price and check if valid.")
+    }
+}
+
+console.log(candyStore('Choco Chunks', 'small'));
+console.log(candyStore('Crackle Crisps','large'));
+console.log(candyStore('All My Toffee', 'jumbo'));
+console.log(candyStore('Mint Chip Supreme', 'lArGe'));
+console.log(candyStore('Darkest Cocoa', 'snack'));
